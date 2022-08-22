f3724c563d59a13be82ac00e0d4eca04d603e771
yyr <dbsdudfh01@korea.ac.kr>
2022-08-22 21:07:07 +0900

    .

--- service/backend/api/types/response.py
+++ service/backend/api/types/response.py
@@ -1,2 +1,24 @@
 from pydanic import BaseModel
-from 
+
+class Video(BaseModel):
+    width=float
+    height=float
+    most_danger_obj=List[Object]
+
+
+
+class Log(BaseModel):
+    bboxs: List[BBOX]
+    img_danger: int # 1 - 3
+    comming_object: List[int] # object의 index
+
+
+class Object(BaseModel):
+    # center 좌표
+    x=float
+    y=float
+    w=float
+    h=float
+    obj_danger = int # 1 - 3
+    obj_class: string
+
