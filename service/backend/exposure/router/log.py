40c3d5857cde79bb798317d578001c9a4a1ef693
yyr <dbsdudfh01@korea.ac.kr>
2022-08-25 17:11:44 +0900

    .

--- service/backend/exposure/router/log.py
+++ service/backend/exposure/router/log.py
@@ -1,28 +1,76 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from datetime import datetime
 
 from common.types import *
+from model.utils.mymodel import MyModel
 
+model = MyModel()
 
 router = APIRouter(prefix='/log', tags=['log'],)
 
-@router.get('/', response_model=Log)
-async def get_root():
+@router.post('/{title}/start', response_model=Video)
+async def start_video(title, response:Response):
     """
-    Return default log
+    카메라는 계속 사진을 찍고 있도록 만든다.
     """
-    return Log(recorded=datetime.now(), objects=[], risked=[], risk=RiskCategory.LOW)
+    global videos
+    videos = await camera.get_videos()
 
-@router.get('/{title}')
-async def get_title(title):
-    """
-    Return a specific log
-    """
-    return title
+    if title not in videos:
+        videos[title] = await camera.start_video(title, 640, 640)
 
-@router.get('/streaming')
-async def get_stream():
+    return videos[title]
+
+
+@router.post('/{title}/end', response_model=Video)
+async def end_video(title, response:Response):
+    global videos
+    if title not in videos:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return None
+    else:
+        videos[title] = await camera.end_video(videos[title]) # duration update
+        return videos[title]
+
+@router.post('/{title}/checkpoint', response_model=boolean)
+async def make_checkpoint(title, response:Response):
     """
-    Return a latest log
+    Save latest frame for logging
     """
-    return None
+    global videos
+    if title not in videos or len(videos[title].frames) == 0:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return None
+    else:
+        return await camera.save_frame(videos[title], videos[title].frames[-1])
+
+
+@router.get('/{title}/streaming', response_model=Image)
+async def get_stream(title:str, response:Response):
+    """
+    Take a picture
+    모델을 돌려서 bbox 예측도 필요!
+    """
+    global videos
+    if title not in videos.keys():
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return None
+    else:
+        img:Image = await camera.capture(videos[title])
+        videos[title].frames.append(img)
+        return img
+
+
+@router.get('/{title}/{frame_id}', response_model=Image)
+async def get_an_image(title, frame_id:int, response:Response):
+    """
+    Return a specific image
+    """
+    global videos
+    if title not in videos.keys() or int(frame_id) < 0 or int(frame_id) >= len(videos[title].frames):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return None
+    else:
+        img:Image = await camera.get_frame(videos[title], frame_id)
+        videos[title].frames.append(img)
+        return img
---
+++ service/backend/model/utils/mymodel.py
@@ -0,0 +1,11 @@
+from common.types import *
+
+
+class MyModel:
+    def __init__(self):
+        pass
+
+    def run(self, x)->Log:
+        """
+        Log를 반환
+        """
